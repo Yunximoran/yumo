f51c2423297b2b7d199111c00783c71c77226383
yumo <2970368069@qq.com>
2024-05-08 18:48:56 +0800

    更改

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,18 +1,42 @@
-import os
-import xml.etree.ElementTree as et
-
+# import xml.etree.ElementTree as et
+# import logging
+# import colorlog
+# import os
 #
-# def loadConf(tree, *args):
-#     """
-#         导入配置文件
-#     :param tree:
-#     :return:
-#     """
-#     root = tree.getroot()
-#     ProjectConf = root.find("project")  # 获取项目配置节点
 #
-#     ProjectAbsPath = ProjectConf.get("path")  # 获取项目根目录
-#     libPath = f"{ProjectAbsPath}\\lib"
+# class LogsManager:
+#     workPath = os.getcwd()
+#     baseName = os.path.basename(workPath)
 #
-#     return libPath
-
+#     __colors = {
+#         "DEBUG": "blue",
+#         "INFO": "green",
+#         "WARNING": "yellow",
+#         "ERROR": "red",
+#         "CRITICAL": "red"
+#     }
+#
+#     def __init__(self, name, attr):
+#         self.logger = logging.getLogger(name)
+#         self.logger.setLevel(logging.DEBUG)
+#
+#         console_handler = logging.StreamHandler()
+#         file_handler = logging.FileHandler(f'{name}.logs', 'w')
+#
+#         logs_format = f"%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+#         # formatter = logging.Formatter(logs_format)
+#         formatter = colorlog.ColoredFormatter(logs_format, log_colors=self.__colors)
+#
+#         console_handler.setFormatter(formatter)
+#
+#         self.logger.addHandler(console_handler)
+#         self.logger.addHandler(file_handler)
+#
+#
+# if __name__ == '__main__':
+#     logger = LogsManager('yumo', 'YuMo\\lib\\LogsManager')
+#     logger.logger.debug('Hello')
+#     logger.logger.info('Hello')
+#     logger.logger.warning('Hello')
+#     logger.logger.error('Hello')
+#     logger.logger.critical('Hello')
--- yumo.py
+++ yumo.py
@@ -1,10 +1,7 @@
-# 导入自建模块
-import time
-
 from lib.build.buildStructure import BuildStructure
 
 # 导入内置模块
-import xml.etree.ElementTree as et
+from lxml import etree
 import subprocess
 import json
 import sys
@@ -12,46 +9,55 @@ import os
 
 args = sys.argv  # args获取参数
 
+# 切换当前目录为工作目录
+workPath = os.path.dirname(__file__)    # dirname获取文件所在目录， __file__获取文件绝对路径
+                                        # dirname 包含 __file__
+os.chdir(workPath)
 
-class Start:
-    # __tree = et.parse('yumo.xml')
-    # __root = __tree.getroot()
 
-    def __init__(self, *args, attr="main"):
-        # 可以启动的程序应该都在main中
-        # self.UpdateProject()
-        self.config()
+class Run:
+    __tree = etree.parse('yumo.xml')
+    __root = __tree.getroot()
+
+    def __init__(self, *args, attr="main"):     # 初始化进程， 定位启动目标
+        # 输入项目名称， 怎样准确定位到文件所在为止
+        self.config(attr)
         self.attr = attr
         label = input("启动项目分类: ")
         startproject = input("启动项目名称: ")
         print(f"running: {startproject}")
         self.start(startproject, label)
 
-    def start(self, name, label, *args):    # 启动项目名称
-        itemPath = os.getcwd() + f"\\{self.attr}\\{label}\\{name}.py"
-        print("project working")
-        time.sleep(10)
+    def start(self, name, label, *args):  # 启动项目
+        itemPath = f"{self.attr}\\{label}\\{name}\\yumo.py"
+        print(f"project working {itemPath}")
+        # self.__changeWorkEnvironment(itemPath)  # 切换工作环境
+        OutPut = subprocess.Popen(['python', itemPath], shell=True, encoding='utf-8')
+        print(OutPut.communicate()[0])  # 获取输出结果
         print("project finish")
 
+    def close(self):
+        pass
 
-    def config(self):
-        self.path = os.getcwd()
+    def config(self, attr):
+        # 获取项目配置
+        Conf = self.__root.find()
+
+
 
     def UpdateProject(self):  # 更新项目
         self.__upxml()
         self.__getStructure()
 
-    @staticmethod
-    def __upxml():
+    def __upxml(self):
         """
             更新项目配置文件
         :return:
         """
-        tree = et.parse('yumo.xml')
-        root = tree.getroot()
-        project = root.find("project")
+
+        project = self.__root.find("project")
         project.set('path', os.getcwd())
-        tree.write('yumo.xml', encoding='utf-8', method='xml')
+        self.__tree.write('yumo.xml', encoding='utf-8', method='xml')
 
     @staticmethod
     def __getStructure(loadPath=os.getcwd()):
@@ -67,4 +73,4 @@ class Start:
             json.dump(frame, f, indent=4, ensure_ascii=False)
 
 
-Start(*args)
+Run()
